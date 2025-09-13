8cce1845bf45f025ceffeb86820a72f44fcfff98
chomee67-lodps <chomee67@lodps.ttct.edu.tw>
2025-09-13 11:06:05 +0800

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,90 @@
+        const lessonData  = [
+            {
+                text: '做人做事做長久',
+                children: [
+                    {
+                        text: '背景',
+                        children: [
+                            {
+                                text: '人物',
+                                children: [{ text: '作者、冷氣小師傅' }]
+                            },
+                            {
+                                text: '事件',
+                                children: [{ text: '小師傅到作者家安裝冷氣' }]
+                            },
+                            {
+                                text: '狀況',
+                                children: [{ text: '獨立安裝費力費工夫' }]
+                            }
+                        ]
+                    },
+                    {
+                        text: '安裝流程',
+                        children: [
+                            {
+                                text: '前置作業',
+                                children: [{ text: '將工具依序排好' }]
+                            },
+                            {
+                                text: '作業過程',
+                                children: [
+                                    { text: '井然有序' },
+                                    { text: '動作不熟練卻準確無誤' }
+                                ]
+                            }
+                        ]                        
+                    },
+                    {
+                        text: '作者期許',
+                        children: [
+                            {
+                                text: '遇見小師傅成功',
+                                children: [
+                                    { text: '精確、完善工作態度' },
+                                    { text: '服務精神' },
+                                    { text: '累積長久經驗' }
+
+                                ]
+                            }
+                        ]                        
+                    },
+                    {
+                        text: '作者反思',
+                        children: [
+                            {
+                                text: '現代人錯誤想法',
+                                children: [
+                                    { text: '講求快速' },
+                                    { text: '不顧周全與體貼' },
+                                    { text: '不留後路' }
+
+                                ]
+                            }
+                        ]                        
+                    },
+                    {
+                        text: '服務精神',
+                        children: [
+                            {
+                                text: '資深師傅',
+                                children: [
+					{ text: '技高人散漫' },
+					{ text: '不關我事' }
+				]
+                            },
+                            {
+                                text: '小師傅',
+                                children: [
+					{ text: '重視顧客的口碑' },
+					{ text: '頭尾都顧到' }
+				]
+                            }
+                        ]
+                    }
+
+
+                ]
+            }
+        ];
+
